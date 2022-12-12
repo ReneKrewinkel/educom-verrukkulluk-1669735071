401b98b144a986e81f1b773034c77d54fe64cb7c
Randy Hamilton <113764657+hamiljoen@users.noreply.github.com>
2022-12-12 09:02:07 +0100

    Opdracht #12

--- index3.php
+++ index3.php
@@ -1,9 +1,23 @@
 <?php
+
+require_once("lib/database.php");
+require_once("lib/artikel.php");
+require_once("lib/user.php");
+require_once("lib/keukentype.php");
+require_once("lib/ingredient.php");
+require_once("lib/gerechtinfo.php");
+require_once("lib/gerecht.php");
+require_once("lib/boodschappen.php");
+
+/// INIT
+$db = new database();
+
 //// Allereerst zorgen dat de "Autoloader" uit vendor opgenomen wordt:
 require_once("./vendor/autoload.php");
 
 /// Twig koppelen:
 $loader = new \Twig\Loader\FilesystemLoader("./templates");
+
 /// VOOR PRODUCTIE:
 /// $twig = new \Twig\Environment($loader), ["cache" => "./cache/cc"]);
 
@@ -14,26 +28,8 @@ $twig->addExtension(new \Twig\Extension\DebugExtension());
 /******************************/
 
 /// Next step, iets met je data doen. Ophalen of zo
-require_once("lib/database.php");
-require_once("lib/artikel.php");
-require_once("lib/user.php");
-require_once("lib/keukentype.php");
-require_once("lib/ingredient.php");
-require_once("lib/gerechtinfo.php");
-require_once("lib/gerecht.php");
-require_once("lib/boodschappen.php");
-
-$db = new database();
-$art = new artikel($db->getConnection());
-$usr = new user($db->getConnection());
-$keu = new keukentype($db->getConnection());
-$ing = new ingredient($db->getConnection());
-$inf = new gerechtinfo($db->getConnection());
-$ger = new gerecht($db->getConnection());
-$lij = new boodschappen($db->getConnection());
-$data = $ger->selecteerGerecht(3);
-// echo "<pre>";
-// var_dump($data);
+$gerecht = new gerecht($db->getConnection());
+//$data = $gerecht->selecteerGerecht();
 
 
 /*
@@ -48,8 +44,8 @@ $action = isset($_GET["action"]) ? $_GET["action"] : "homepage";
 switch($action) {
 
         case "homepage": {
-            $data = $ger->selecteerGerecht();
-            $template = 'detail.html.twig';
+            $data = $gerecht->selecteerGerecht();
+            $template = 'homepage.html.twig';
             $title = "homepage";
             break;
         }
